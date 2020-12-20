5ab03a4d9c222579a701bf6859ee8ac453af804b
kaino <kaino@yadokari.tv>
2020-12-20 12:38:57 +0900

    first commit

---
+++ src/Entity/Pokemon.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\PokemonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=PokemonRepository::class)
+ * @ApiResource
+ */
+class Pokemon
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
